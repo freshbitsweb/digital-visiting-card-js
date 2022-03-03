0b97b98f70f0544bcf542ea2474635dcd815177a
misusonu18 <misusonu18@gmail.com>
2022-03-03 16:13:19 +0530

    code: updated the api

--- functions/create.js
+++ functions/create.js
@@ -1,30 +1,48 @@
 const axios = require("axios");
 exports.handler = async function (event, context) {
-  try {
-    var data = event.body;
-    axios
-      .put(
-        "https://api.github.com/repos/misusonu18/digital-visiting-card-js/contents/js",
-        JSON.stringify(data)
-      )
-      .then(function (res) {
-        console.log(res);
-      });
+	try {
+		var response;
+		const d = new Date();
+		let time = d.getTime();
+		var data1 = JSON.parse(event.body);
+		var data = JSON.stringify({
+			"message": "created the file...",
+			"content": btoa(JSON.stringify(data1)),
+		});
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        msg: "Completed Successfully",
-      }),
-    };
-  } catch (err) {
-    console.log(err.message);
+		let name = (data1.first_name).replace(" ", "_");
+		let fileName = name + time + ".json";
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        msg: err.message,
-      }),
-    };
-  }
+		var config = {
+			method: 'put',
+			url: 'https://api.github.com/repos/misusonu18/digital-visiting-card-js/contents/js/json/' + fileName,
+			headers: {
+				'Authorization': 'Bearer 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',
+				'Content-Type': 'application/json'
+			},
+			data: data
+		}
+
+		axios(config)
+			.then(function (response) {
+				response = response;
+			})
+			.catch(function (error) {
+				response = error;
+			});
+
+		return {
+			statusCode: 201,
+			body: JSON.stringify({
+				msg: response,
+			}),
+		};
+	} catch (err) {
+		return {
+			statusCode: 404,
+			body: JSON.stringify({
+				msg: err.message,
+			}),
+		};
+	}
 };
