fd7c52c9c1c990719ecab72393e783925b279147
misusonu18 <misusonu18@gmail.com>
2022-03-03 16:58:39 +0530

    code: update theheaders

--- functions/create.js
+++ functions/create.js
@@ -18,7 +18,6 @@ exports.handler = async function (event, context) {
 			method: 'put',
 			url: 'https://api.github.com/repos/misusonu18/digital-visiting-card-js/contents/js/json/' + fileName,
 			headers: {
-				'Authorization': 'Bearer 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',
 				'Content-Type': 'application/json'
 			},
 			data: data
