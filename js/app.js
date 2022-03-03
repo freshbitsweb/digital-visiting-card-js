aeb6e8b52bee3f8289e81b586661cde459fa95e1
misusonu18 <misusonu18@gmail.com>
2022-03-03 16:13:31 +0530

    code: updated the validations

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-let nameInput = document.getElementById('name');
+let firstNameInput = document.getElementById('first-name');
+let lastNameInput = document.getElementById('last-name');
 let emailInput = document.getElementById('email');
 let phoneNumberInput = document.getElementById('phone-number');
-let addressInput = document.getElementById('address');
+let githubInput = document.getElementById('github-link');
+let websiteInput = document.getElementById('website');
 
-let nameValidation = document.getElementById('name-validation');
+let firstNameValidation = document.getElementById('first-name-validation');
+let lastNameValidation = document.getElementById('last-name-validation');
 let emailValidation = document.getElementById('email-validation');
 let phoneNumberValidation = document.getElementById('phone-number-validation');
-let addressValidation = document.getElementById('address-validation');
+let githubValidation = document.getElementById('github-validation');
+let websiteValidation = document.getElementById('website-validation');
 
 let emailFlag = 0;
-let addressFlag = 0;
-let nameFlag = 0;
+let websiteFlag = 0;
+let firstNameFlag = 0;
+let lastNameFlag = 0;
 let phoneNumberFlag = 0;
+let githubFlag = 0;
 
 let onChangeInput = (idName) => {
     if (idName === 'email') {
@@ -33,33 +39,78 @@ let onChangeInput = (idName) => {
         }
 
     }
-    else if (idName === 'name') {
+    else if (idName === 'first-name') {
         let nameRegex = /^[a-zA-Z ]{2,30}$/;
 
-        let result = (nameInput.value).match(nameRegex);
-        if (nameInput.value === '') {
-            nameValidation.innerText = 'Please Enter Name';
-            nameFlag = 0;
+        let result = (firstNameInput.value).match(nameRegex);
+        if (firstNameInput.value === '') {
+            firstNameValidation.innerText = 'Please Enter Name';
+            firstNameFlag = 0;
         }
         else {
             if (!result) {
-                nameValidation.innerText = 'Invalid First Name';
-                nameFlag = 0;
+                firstNameValidation.innerText = 'Invalid First Name';
+                firstNameFlag = 0;
             }
             else {
-                nameValidation.innerText = '';
-                nameFlag = 1;
+                firstNameValidation.innerText = '';
+                firstNameFlag = 1;
             }
         }
     }
-    else if (idName === 'address') {
-        if (addressInput.value === '') {
-            addressValidation.innerText = 'Please Enter Address';
-            addressFlag = 0;
+    else if (idName === 'last-name') {
+        let nameRegex = /^[a-zA-Z ]{2,30}$/;
+
+        let result = (lastNameInput.value).match(nameRegex);
+        if (lastNameInput.value === '') {
+            lastNameValidation.innerText = 'Please Enter Name';
+            lastNameFlag = 0;
         }
         else {
-            addressValidation.innerText = '';
-            addressFlag = 1;
+            if (!result) {
+                lastNameValidation.innerText = 'Invalid First Name';
+                lastNameFlag = 0;
+            }
+            else {
+                lastNameValidation.innerText = '';
+                lastNameFlag = 1;
+            }
+        }
+    }
+    else if (idName === 'github') {
+        let regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+
+        let result = (githubInput.value).match(regex);
+        if (githubInput.value === '') {
+            githubValidation.innerText = 'Please Enter Github Id';
+            githubFlag = 0;
+        }
+        else {
+            if (!result) {
+                githubValidation.innerText = '';
+                    githubFlag = 1;
+            }
+            githubValidation.innerText = '';
+            githubFlag = 1;
+        }
+    }
+    else if (idName === 'website') {
+        let regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+
+        let result = (websiteInput.value).match(regex);
+        if (websiteInput.value === '') {
+            websiteValidation.innerText = 'Please Enter Website';
+            websiteFlag = 0
+        }
+        else {
+            if (!result) {
+                websiteValidation.innerText = 'Invalid Website';
+                websiteFlag = 0
+            }
+            else {
+                websiteValidation.innerText = '';
+                websiteFlag = 1;
+            }
         }
     }
     else if (idName === 'phone-number') {
@@ -83,18 +134,21 @@ let onChangeInput = (idName) => {
     }
 };
 
-let submitForm = () => {
-    alert("welcome");
+let submitForm = async() => {
     let jsonData = {
-        'name': nameInput.value,
+        'first_name': firstNameInput.value,
+        'last_name': lastNameInput.value,
         'email': emailInput.value,
         'phone-number': phoneNumberInput.value,
-        'address': addressInput.value,
+        'website': websiteInput.value,
+        'github': githubInput.value,
     }
 
     axios.post(
-        '/create',
-        jsonData
+        '/.netlify/functions/create',
+        jsonData, {
+        'Content-Type': 'application/json'
+        }
     ).then((res) => {
         console.log(res);
     }).catch((err) => {
