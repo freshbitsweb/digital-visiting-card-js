be77b3007d069cc9a6fd3cd3f41d2d2955651ea3
misusonu18 <misusonu18@gmail.com>
2022-03-07 19:37:36 +0530

    js

---
+++ js/getAllData.js
@@ -0,0 +1,27 @@
+axios.post(
+    '/.netlify/functions/read',
+).then((res) => {
+    let datas = res.data.data;
+    datas.forEach(element => {
+        let name = element.name;
+        let html = `<div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h6 class="card-title fs-6">`+ name +`</h6>
+                <button class="btn btn-outline-primary" onclick="getTheFileData('`+name+`')">click</button>
+            </div>
+        </div>`;
+
+        document.getElementById('row').innerHTML += html;
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+function getTheFileData(fileName) {
+    axios.post(
+        '/.netlify/functions/readOneFile',
+        fileName
+    ).then((res) => {
+        sessionStorage.setItem("fileData", atob(res.data.data.content));
+    });
+}
