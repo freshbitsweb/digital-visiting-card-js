ec72e81dc6e8b6d3b713ed0715ec4c52d68a2e40
misusonu18 <misusonu18@gmail.com>
2022-03-10 17:22:54 +0530

    update the navbar

--- js/app.js
+++ js/app.js
@@ -1,29 +1,3 @@
-var navbarHtml = `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Digital Visiting Card</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-				data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-				aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="index.html">Home</a>
-					</li>
-
-					<li class="nav-item">
-						<a class="nav-link d-hidden" href="form.html">Form</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-`;
-// let body = document.getElementById('body');
-// body.innerHTML = navbarHtml;
-
 let netlifyIdentityLogout = () => {
 	netlifyIdentity.logout();
 };
@@ -42,3 +16,38 @@ let checkAuth = () => {
 }
 
 checkAuth();
+
+var navbarHtml = `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Digital Visiting Card</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+				data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link d-hidden" href="manage.html">Form</a>
+                    </li>
+                </ul>
+				<div class="dropdown ms-auto">
+					<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+						<i class="fa fa-user"></i>
+						<span id="login-username">${userEmail}</span>
+					</button>
+					<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+						<li><a class="dropdown-item" href="#" onclick="netlifyIdentityLogout()">Logout</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
+`;
+let body = document.getElementById('dynamic-body');
+body.innerHTML = navbarHtml;
