b2bd810250ac5d0cb36243d2bdddb1cbbdc27b84
misusonu18 <misusonu18@gmail.com>
2022-03-14 19:43:27 +0530

    code: update the col

--- js/getAllData.js
+++ js/getAllData.js
@@ -22,7 +22,7 @@ axios.post(
     allData.forEach(element => {
         let name = element.name;
         let html = `
-            <div class="col-12 col-lg-3 col-md-4 col-sm-6">
+            <div class="col-12 col-lg-4 col-md-4 col-sm-6">
                 <div class="card mt-4" style="width: 18rem;">
                     <div class="card-body">
                         <h6 class="card-title">${name}</h6>
