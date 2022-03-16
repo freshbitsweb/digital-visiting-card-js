bb70e3f3961148b7c401cdebc77659ae08e9a8b8
misusonu18 <misusonu18@gmail.com>
2022-03-16 16:52:27 +0530

    code: using direct getElementById

--- js/card.js
+++ js/card.js
@@ -1,23 +1,12 @@
-let card = document.getElementById('card');
-let firstField = document.getElementById('first');
-let lastField = document.getElementById('last');
-let titleField = document.getElementById('title');
-let websiteField = document.getElementById('website');
-let gmailField = document.getElementById('gmail');
-let githubField = document.getElementById('github');
-let phoneNumberField = document.getElementById('phone-number');
-let whatsapp = document.getElementById('whatsapp');
-
-
-getFileData = sessionStorage.getItem("file-data-card");
-getFileData = JSON.parse(getFileData);
-firstField.innerHTML = getFileData.first_name;
-lastField.innerHTML = getFileData.last_name;
-titleField.innerHTML = getFileData.title ? getFileData.title : '';
-gmailField.innerHTML = getFileData.email;
-phoneNumberField.innerHTML = getFileData.phone_number;
-whatsapp.href = 'https://wa.me/' + getFileData.phone_number;
-phoneNumberField.href = 'tel:' + getFileData.phone_number;
-gmailField.href = 'mailto:' + getFileData.email;
-githubField.href = getFileData.github;
-websiteField.href = getFileData.website
+let visitingCardData= sessionStorage.getItem("visiting-card-data");
+visitingCardData = JSON.parse(visitingCardData);
+document.getElementById('first').innerHTML = visitingCardData.first_name;
+document.getElementById('last').innerHTML = visitingCardData.last_name;
+document.getElementById('title').innerHTML = visitingCardData.title ? visitingCardData.title : '';
+document.getElementById('gmail').innerHTML = visitingCardData.email;
+document.getElementById('phone-number').innerHTML = visitingCardData.phone_number;
+document.getElementById('whatsapp').href = 'https://wa.me/' + visitingCardData.phone_number;
+document.getElementById('phone-number').href = 'tel:' + visitingCardData.phone_number;
+document.getElementById('gmail').href = 'mailto:' + visitingCardData.email;
+document.getElementById('github').href = visitingCardData.github;
+document.getElementById('website').href = visitingCardData.website
