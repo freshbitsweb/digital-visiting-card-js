28b5e1fa5f539e3985c9a98ec957ba3e64afbc0b
misusonu18 <misusonu18@gmail.com>
2022-03-22 16:16:52 +0530

    code: added the params url and changed the last name and first name

--- js/card.js
+++ js/card.js
@@ -1,7 +1,9 @@
 let visitingCardData= sessionStorage.getItem("visiting-card-data");
 visitingCardData = JSON.parse(visitingCardData);
-document.getElementById('first').innerHTML = visitingCardData.first_name;
-document.getElementById('last').innerHTML = visitingCardData.last_name;
+let firstName = (visitingCardData.name).split(' ')[0];
+let lastName = (visitingCardData.name).split(' ')[1];
+document.getElementById('first').innerHTML = firstName;
+document.getElementById('last').innerHTML = lastName;
 document.getElementById('title').innerHTML = visitingCardData.title ? visitingCardData.title : '';
 document.getElementById('gmail').innerHTML = visitingCardData.email;
 document.getElementById('phone-number').innerHTML = visitingCardData.phone_number;
@@ -9,4 +11,40 @@ document.getElementById('whatsapp').href = 'https://wa.me/' + visitingCardData.p
 document.getElementById('phone-number').href = 'tel:' + visitingCardData.phone_number;
 document.getElementById('gmail').href = 'mailto:' + visitingCardData.email;
 document.getElementById('github').href = visitingCardData.github;
-document.getElementById('website').href = visitingCardData.website
+document.getElementById('website').href = visitingCardData.website;
+
+let queryString = document.location.search;
+let paramString = queryString.split('?')[1];
+let params_arr = paramString.split('&');
+
+const fetchSpecificCard = async (fileName, folderName) => {
+    await axios.post(
+        '/.netlify/functions/getSpecificVisitingCard', {
+        file_name: fileName,
+        folder_name: folderName
+    }
+    ).then((res) => {
+        let data = atob(res.data.data.content);
+        sessionStorage.removeItem("visiting-card-data");
+        sessionStorage.setItem("visiting-card-data", data);
+    });
+};
+
+const fetchVisitingCards = async (phoneNumber) => {
+    await axios.post(
+        '/.netlify/functions/fetchPhoneNumbers'
+    ).then((res) => {
+        let phoneNumberArray = JSON.parse(window.atob(res.data.data.content))
+        phoneNumberArray.forEach(element => {
+            if (element.phone_number == phoneNumber) {
+                fetchSpecificCard(element.file_name, element.folder_name);
+                return;
+            }
+        });
+    });
+}
+
+for (let i = 0; i < params_arr.length; i++) {
+    let pair = params_arr[i].split('=');
+    fetchVisitingCards(pair[1]);
+}
