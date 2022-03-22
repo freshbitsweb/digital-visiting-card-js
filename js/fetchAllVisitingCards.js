e740a801a7dbd3c3007883623c2dc816d5bb873c
misusonu18 <misusonu18@gmail.com>
2022-03-22 16:17:10 +0530

    code: refactor

--- js/fetchAllVisitingCards.js
+++ js/fetchAllVisitingCards.js
@@ -1,58 +1,64 @@
-displayLoading();
 let userName = localStorage.getItem('username');
 const templateWithData = document.getElementById('card-with-data');
 const templateWithoutData = document.getElementById('card-without-data');
 const row = document.getElementById('row');
 
-axios.post(
-    '/.netlify/functions/getVisitingCards', {
+const fetchAllCards = async () => {
+    displayLoading();
+    await axios.post(
+        '/.netlify/functions/getVisitingCards', {
         folder_name: userName
     }
-).then((res) => {
-    let allData = res.data.data;
-    if (allData.status == 404) {
-        const card = templateWithoutData.content.cloneNode(true);
-        hideLoading();
-        row.append(card);
-        return;
-    }
-
-    hideLoading();
-    allData.forEach(element => {
-        let name = element.name;
-        const card = templateWithData.content.cloneNode(true);
-        card.querySelector('.card-name').innerText = name;
-        card.querySelector('.click-card-button').onclick = function() { getTheFileData(name) };
-        card.querySelector('.edit-card-button').onclick = function() { editTheFileData(name) };
-
-        row.append(card);
-    });
-}).catch((err) => {});
-
-function getTheFileData(fileName) {
-    displayLoading();
-    axios.post(
-        '/.netlify/functions/getSpecificVisitingCard', {
-            file_name: fileName,
-            folder_name: userName
+    ).then((response) => {
+        let allData = response.data.data;
+        if (allData.status == 404) {
+            const card = templateWithoutData.content.cloneNode(true);
+            hideLoading();
+            row.append(card);
+            return;
         }
+
+        hideLoading();
+        allData.forEach(element => {
+            let filename = element.name;
+            let name = filename.split('_');
+            const card = templateWithData.content.cloneNode(true);
+            card.querySelector('.card-name').innerText = name[0] + ' ' + name[1];
+            card.querySelector('.click-card-button').onclick = function () { getTheFileData(filename) };
+            card.querySelector('.edit-card-button').onclick = function () { editTheFileData(filename) };
+
+            row.append(card);
+        });
+    });
+};
+
+fetchAllCards();
+
+const getTheFileData = async (fileName) => {
+    displayLoading();
+    await axios.post(
+        '/.netlify/functions/getSpecificVisitingCard', {
+        file_name: fileName,
+        folder_name: userName
+    }
     ).then((res) => {
         let data = atob(res.data.data.content);
         sessionStorage.setItem("visiting-card-data", data);
         location.replace('card.html');
     });
-}
+};
 
-function editTheFileData(fileName) {
+const editTheFileData = async (fileName) => {
     displayLoading();
-    axios.post(
+    await axios.post(
         '/.netlify/functions/getSpecificVisitingCard', {
-            file_name: fileName,
-            folder_name: userName
-        }
+        file_name: fileName,
+        folder_name: userName
+    }
     ).then((res) => {
         let data = res.data.data;
+        sessionStorage.removeItem("visiting-card-data");
         sessionStorage.setItem("visiting-card-data", JSON.stringify(data));
         location.replace('manage.html');
     });
-}
+};
