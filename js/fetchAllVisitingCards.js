13e502816b57f503a65f3d299398b8facf5b4a2d
misusonu18 <misusonu18@gmail.com>
2022-04-09 19:53:40 +0530

    code: update the alertify

--- js/fetchAllVisitingCards.js
+++ js/fetchAllVisitingCards.js
@@ -37,6 +37,7 @@ const fetchAllCards = async () => {
                 hideLoading();
                 const data = JSON.parse(atob(res.data.data.content));
                 const card = templateWithData.content.cloneNode(true);
+                let generatedLink = window.location.href + 'card.html?phone_number = ' + data.phone_number;
 
                 card.querySelector('.card-name').innerHTML = data.name;
                 card.querySelector('.card-title').innerHTML = data.title ? data.title : '';
@@ -48,7 +49,19 @@ const fetchAllCards = async () => {
                 card.querySelector('.card-website').href = data.website;
                 card.querySelector('.card-github').innerHTML = data.github;
                 card.querySelector('.card-github').href = data.github;
-                card.querySelector('.edit-card-button').onclick = function () { editTheFileData(element.name) }
+                card.querySelector('.edit-card-button').onclick = function () { editTheFileData(element.name) };
+                card.querySelector('.open-card-button').onclick = function () { getTheFileData(element.name) };
+                card.querySelector('.click-card-button').onclick = function () {
+                    console.log('hello');
+                    card.querySelector('.click-card-button').setAttribute('data-bs-target', '#share-div-modal-' + data.name);
+                };
+                card.querySelector('.copy-link').onclick = function () {
+                    navigator.clipboard.writeText(generatedLink);
+                    alertify.success('Link Copied Successfully.');
+                };
+                card.querySelector('.twitter-link').onclick = function () {
+                    window.location.reload('https://twitter.com/intent/tweet?text=Digital Visiting Card of '+ data.name +' &url=' + generatedLink);
+                };
                 row.append(card);
             });
         });
@@ -69,5 +82,19 @@ const editTheFileData = async (fileName) => {
         sessionStorage.removeItem("visiting-card-data");
         sessionStorage.setItem("visiting-card-data", JSON.stringify(data));
         location.replace('manage.html');
+    });
+};
+
+const getTheFileData = async (fileName) => {
+    displayLoading();
+    await axios.post(
+        '/fetch-specific-visiting-card', {
+        file_name: fileName,
+        folder_name: userName
+    }
+    ).then((res) => {
+        let data = atob(res.data.data.content);
+        sessionStorage.setItem("visiting-card-data", data);
+        location.replace('card.html');
     });
 };
